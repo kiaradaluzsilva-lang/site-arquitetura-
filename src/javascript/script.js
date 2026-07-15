aa13a794b368c7023c69cf1f21e60f8e65b29bf4
KiaraLuz <kiaradaluzsilva@gmail.com>
2026-07-15 18:04:53 -0300

    Add files via upload

---
+++ src/javascript/script.js
@@ -0,0 +1,58 @@
+// ===============================
+// MENU MOBILE
+// ===============================
+
+const menuButton = document.querySelector("#menu-button");
+const menuIcon = menuButton.querySelector("i");
+const mobileMenu = document.querySelector("#mobile-menu");
+const mobileLinks = document.querySelectorAll("#mobile-menu a");
+
+menuButton.addEventListener("click", () => {
+
+    mobileMenu.classList.toggle("active");
+
+    if (mobileMenu.classList.contains("active")) {
+        menuIcon.classList.remove("ri-menu-3-line");
+        menuIcon.classList.add("ri-close-line");
+    } else {
+        menuIcon.classList.remove("ri-close-line");
+        menuIcon.classList.add("ri-menu-3-line");
+    }
+
+});
+
+// Fecha o menu ao clicar em um link
+
+mobileLinks.forEach(link => {
+
+    link.addEventListener("click", () => {
+
+        mobileMenu.classList.remove("active");
+
+        menuIcon.classList.remove("ri-close-line");
+        menuIcon.classList.add("ri-menu-3-line");
+        menuButton.setAttribute(
+            "aria-label",
+            mobileMenu.classList.contains("active")
+            ? "Fechar menu de navegação" 
+            : "Abrir menu de navegação"
+         );
+        
+    });
+
+});
+
+// Fecha o menu ao voltar para desktop
+
+window.addEventListener("resize", () => {
+
+    if (window.innerWidth > 900) {
+
+        mobileMenu.classList.remove("active");
+
+        menuIcon.classList.remove("ri-close-line");
+        menuIcon.classList.add("ri-menu-3-line");
+
+    }
+
+});
